876189ef1f50a5b29765e09da79b99c3c25d05fc
Olawale <bamideleolawale13@gmail.com>
2022-12-23 01:25:22 +0100

    styling

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Hompage.css'
+import Directory from "../../component/directory/Directory";
+
+const Homepage = () => {
+  return (
+    <>
+      <div className='homepage'>
+      <Directory />
+      </div>
+    </>
+  );
+};
+
+export default Homepage;
